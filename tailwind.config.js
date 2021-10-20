48e5a33f276321e4723b02365bbd3fe2b7a3afc4
Jonathan Tarnate <jstarnate16@gmail.com>
2021-10-20 18:12:05 +0800

    Fixed the theming

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,19 +9,46 @@ module.exports = {
             lg: '1024px',
             xl: '1366px',
         },
-        backgroundColor: {
+        colors: {
+            primary: {
+                lighter: 'rgba(17, 123, 182, .1)',
+                light: 'rgba(17, 123, 182, .3)',
+                DEFAULT: 'rgba(36, 142, 201, .8)',
+                dark: 'rgb(36, 142, 201)',
+            },
+            success: {
+                lighter: 'rgba(34, 157, 46, .1)',
+                light: 'rgba(34, 157, 46, .3)',
+                DEFAULT: 'rgba(51, 171, 62, .8)',
+                dark: 'rgb(51, 171, 62)',
+            },
+            danger: {
+                lighter: 'rgba(182, 17, 17, .1)',
+                light: 'rgba(229, 59, 59, .3)',
+                DEFAULT: 'rgba(229, 59, 59, .8)',
+                dark: 'rgb(229, 59, 59)',
+            },
             skin: {
-                'contrast-light': 'var(--bg-color-contrast-light)',
-                contrast: 'var(--bg-color-contrast)',
-                DEFAULT: 'var(--bg-color-default)',
+                bg: {
+                    'contrast-light': 'var(--skin-bg-contrast-light)',
+                    contrast: 'var(--skin-bg-contrast)',
+                    DEFAULT: 'var(--skin-bg-default)',
+                },
+                text: {
+                    light: 'var(--skin-text-light)',
+                    DEFAULT: 'var(--skin-text-default)',
+                    dark: 'var(--skin-text-dark)',
+                },
             },
         },
-        textColor: {
-            skin: {
-                light: 'var(--text-color-light)',
-                DEFAULT: 'var(--text-color-default)',
-                dark: 'var(--text-color-dark)',
-            },
+        fontFamily: {
+            sans: ['Roboto', 'sans-serif'],
+        },
+        fontSize: {
+            lg: '20px',
+            md: '16px',
+            sm: '14px',
+            xs: '12px',
         },
         spacing: {
             xxl: '40px',
@@ -31,28 +58,7 @@ module.exports = {
             sm: '10px',
             xs: '5px',
         },
-        extend: {
-            colors: {
-                primary: {
-                    lighter: 'rgba(17, 123, 182, .1)',
-                    light: 'rgba(17, 123, 182, .3)',
-                    DEFAULT: 'rgba(36, 142, 201, .8)',
-                    dark: 'rgb(36, 142, 201)',
-                },
-                success: {
-                    lighter: 'rgba(34, 157, 46, .1)',
-                    light: 'rgba(34, 157, 46, .3)',
-                    DEFAULT: 'rgba(51, 171, 62, .8)',
-                    dark: 'rgb(51, 171, 62)',
-                },
-                danger: {
-                    lighter: 'rgba(182, 17, 17, .1)',
-                    light: 'rgba(229, 59, 59, .3)',
-                    DEFAULT: 'rgba(229, 59, 59, .8)',
-                    dark: 'rgb(229, 59, 59)',
-                },
-            },
-        },
+        extend: {},
     },
     variants: {
         extend: {},
