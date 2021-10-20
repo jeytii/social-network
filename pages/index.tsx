e3eb43febf13a2ad48c18dd51c2256a4acc1b5a2
Jonathan Tarnate <jstarnate16@gmail.com>
2021-10-20 18:12:51 +0800

    Initialized the login page's UI.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,85 +1,40 @@
 import Head from 'next/head';
+import Logo from 'components/Logo';
+import InputField from 'components/utilities/InputField';
+import { BiErrorCircle } from 'react-icons/bi';
 
 export default function Home() {
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <section className="bg-skin-bg h-screen">
             <Head>
-                <title>Create Next App</title>
+                <title>Welcome to sosyal.me</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-                <h1 className="text-6xl font-bold">
-                    Welcome to
-                    {' '}
-                    <a className="text-blue-600" href="https://nextjs.org">
-                        Next.js!
-                    </a>
-                </h1>
+            <header className="flex align-middle justify-center py-lg">
+                <Logo />
+            </header>
 
-                <p className="mt-3 text-2xl">
-                    Get started by editing
-                    {' '}
-                    <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-                        pages/index.js
-                    </code>
-                </p>
+            <main className="max-w-screen-sm m-auto rounded-md bg-skin-bg-contrast p-lg">
+                <h1 className="text-lg font-bold text-skin-text-light text-center">Sign in to you account</h1>
 
-                <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-                    <a
-                        href="https://nextjs.org/docs"
-                        className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-                    >
-                        <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-                        <p className="mt-4 text-xl">
-                            Find in-depth information about Next.js features and API.
-                        </p>
-                    </a>
-
-                    <a
-                        href="https://nextjs.org/learn"
-                        className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-                    >
-                        <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-                        <p className="mt-4 text-xl">
-                            Learn about Next.js in an interactive course with quizzes!
-                        </p>
-                    </a>
-
-                    <a
-                        href="https://github.com/vercel/next.js/tree/master/examples"
-                        className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-                    >
-                        <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-                        <p className="mt-4 text-xl">
-                            Discover and deploy boilerplate example Next.js projects.
-                        </p>
-                    </a>
-
-                    <a
-                        href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                        className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-                    >
-                        <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-                        <p className="mt-4 text-xl">
-                            Instantly deploy your Next.js site to a public URL with Vercel.
-                        </p>
-                    </a>
+                <div className="flex items-center bg-danger-lighter text-danger p-md border-danger rounded-md mt-lg">
+                    <BiErrorCircle className="text-lg" />
+                    <span className="ml-sm">Incorrect combination</span>
                 </div>
-            </main>
 
-            <footer className="flex items-center justify-center w-full h-24 border-t">
-                <a
-                    className="flex items-center justify-center"
-                    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Powered by
-                    {' '}
-                    <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-                </a>
-            </footer>
-        </div>
+                <form className="py-lg">
+                    <InputField id="username" label="Username or email address" />
+
+                    <InputField className="mt-md" id="username" type="password" label="Password" />
+
+                    <button type="button" className="btn-primary mt-lg w-full" disabled>Sign in</button>
+                </form>
+
+                <a href="/" className="text-primary no-underline text-center block">Create an account</a>
+
+                <a href="/" className="text-skin-text-light no-underline text-center block mt-sm">Forgot password</a>
+            </main>
+        </section>
     );
 }
