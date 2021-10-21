aed2509e4271067ac9fe6eb81106f584740f3c61
Jonathan Tarnate <jstarnate16@gmail.com>
2021-10-22 01:59:51 +0800

    Made some minor changes.

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,30 +11,32 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <header className="flex align-middle justify-center py-lg">
+            <header className="flex items-center justify-center py-lg">
                 <Logo />
             </header>
 
-            <main className="max-w-screen-sm m-auto rounded-md bg-skin-bg-contrast p-lg">
-                <h1 className="text-lg font-bold text-skin-text-light text-center">Sign in to you account</h1>
+            <div className="py-md sm:px-md">
+                <main className="max-w-screen-sm m-auto rounded-md bg-skin-bg-contrast p-lg">
+                    <h1 className="text-lg font-bold text-skin-text-light text-center">Sign in to your account</h1>
 
-                <div className="flex items-center bg-danger-lighter text-danger p-md border-danger rounded-md mt-lg">
-                    <BiErrorCircle className="text-lg" />
-                    <span className="ml-sm">Incorrect combination</span>
-                </div>
+                    <div className="flex items-center bg-danger-lighter text-danger p-md border-danger rounded-md mt-lg">
+                        <BiErrorCircle className="text-lg" />
+                        <span className="ml-sm">Incorrect combination</span>
+                    </div>
 
-                <form className="py-lg">
-                    <InputField id="username" label="Username or email address" />
+                    <form className="py-lg">
+                        <InputField id="username" type="text" label="Username or email address" autoFocus />
 
-                    <InputField className="mt-md" id="username" type="password" label="Password" />
+                        <InputField containerClassName="mt-md" id="password" type="password" label="Password" />
 
-                    <button type="button" className="btn-primary mt-lg w-full" disabled>Sign in</button>
-                </form>
+                        <button type="button" className="btn-primary mt-lg w-full" disabled>Sign in</button>
+                    </form>
 
-                <a href="/" className="text-primary no-underline text-center block">Create an account</a>
+                    <a href="/" className="text-primary no-underline text-center block">Create an account</a>
 
-                <a href="/" className="text-skin-text-light no-underline text-center block mt-sm">Forgot password</a>
-            </main>
+                    <a href="/" className="text-skin-text-light no-underline text-center block mt-sm">Forgot password</a>
+                </main>
+            </div>
         </section>
     );
 }
