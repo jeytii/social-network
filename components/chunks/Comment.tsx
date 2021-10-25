21697bf80c6939c4fd0b116743ca3d57b88b2f5a
Jonathan Tarnate <jstarnate16@gmail.com>
2021-10-25 13:35:03 +0800

    Initialized view post page's UI.

---
+++ components/chunks/Comment.tsx
@@ -0,0 +1,46 @@
+import { HTMLAttributes } from 'react';
+import clsx from 'clsx';
+import {
+    MdOutlineClear,
+    MdOutlineEditNote,
+    MdOutlineThumbUp,
+} from 'react-icons/md';
+import BasicInfo from 'components/utilities/BasicInfo';
+
+type Props = HTMLAttributes<HTMLElement>
+
+export default function Comment({ className, ...props }: Props) {
+    return (
+        <article className={clsx('bg-skin-bg-contrast rounded-md p-md', className)} {...props}>
+            <BasicInfo imageUrl="/" imageSize={40} gender="Male" name="John doe" username="5 hours ago" />
+
+            <p className="paragraph-sm text-skin-text my-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae pariatur obcaecati recusandae tempore reprehenderit officia aut culpa deleniti ducimus, blanditiis aliquam consequuntur ipsam ullam! Possimus fugit veniam ipsam voluptas id! Lorem ipsum dolor sit amet consectetur adipisicing elitee.</p>
+
+            <div className="flex items-center">
+                <button
+                    className="flex items-center text-skin-text-light hover:text-primary"
+                    type="button"
+                >
+                    <MdOutlineThumbUp className="text-md" />
+                    <span className="text-xs ml-xs">5</span>
+                </button>
+
+                <button
+                    className="flex items-center text-skin-text-light ml-xxl hover:text-skin-text"
+                    type="button"
+                >
+                    <MdOutlineEditNote className="text-lg" />
+                    <span className="text-xs ml-xs">Edit</span>
+                </button>
+
+                <button
+                    className="flex items-center text-skin-text-light ml-xxl hover:text-danger"
+                    type="button"
+                >
+                    <MdOutlineClear className="text-lg" />
+                    <span className="text-xs ml-xs">Delete</span>
+                </button>
+            </div>
+        </article>
+    );
+}
---
+++ pages/post/[slug].tsx
@@ -0,0 +1,20 @@
+import Protected from 'components/Protected';
+import Post from 'components/chunks/Post';
+import Comment from 'components/chunks/Comment';
+import CommentBox from 'components/chunks/CommentBox';
+
+export default function ViewPost() {
+    return (
+        <Protected title="Sosyal.me">
+            <Post />
+
+            <CommentBox />
+
+            <section className="mt-lg">
+                <Comment />
+                <Comment className="mt-lg" />
+                <Comment className="mt-lg" />
+            </section>
+        </Protected>
+    );
+}
