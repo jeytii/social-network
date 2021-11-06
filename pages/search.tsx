9e8bfc0c85ec391764104f4334c0291edb7175f5
Jonathan Tarnate <jstarnate16@gmail.com>
2021-11-06 14:59:53 +0800

    Initialized search users page's UI.

---
+++ pages/search.tsx
@@ -0,0 +1,66 @@
+import { MdPersonAddAlt } from 'react-icons/md';
+import Protected from 'components/Protected';
+import BasicInfo from 'components/utilities/BasicInfo';
+
+export default function Search() {
+    return (
+        <Protected title='Search people - Sosyal.me'>
+            <section className='p-lg'>
+                <h1 className='text-md text-skin-text font-bold'>
+                    Search people
+                </h1>
+
+                <div className='mt-sm'>
+                    <div className='flex items-center bg-skin-bg-contrast rounded-md p-sm'>
+                        <BasicInfo
+                            name='John Doe'
+                            username='@john.doe'
+                            gender='Male'
+                            imageUrl='/'
+                            imageSize={50}
+                        />
+
+                        <button
+                            type='button'
+                            className='rounded-full p-sm ml-auto hover:bg-primary-lighter'
+                        >
+                            <MdPersonAddAlt className='text-lg text-primary' />
+                        </button>
+                    </div>
+                    <div className='flex items-center bg-skin-bg-contrast rounded-md p-sm mt-lg'>
+                        <BasicInfo
+                            name='John Doe'
+                            username='@john.doe'
+                            gender='Male'
+                            imageUrl='/'
+                            imageSize={50}
+                        />
+
+                        <button
+                            type='button'
+                            className='rounded-full p-sm ml-auto hover:bg-primary-lighter'
+                        >
+                            <MdPersonAddAlt className='text-lg text-primary' />
+                        </button>
+                    </div>
+                    <div className='flex items-center bg-skin-bg-contrast rounded-md p-sm mt-lg'>
+                        <BasicInfo
+                            name='John Doe'
+                            username='@john.doe'
+                            gender='Male'
+                            imageUrl='/'
+                            imageSize={50}
+                        />
+
+                        <button
+                            type='button'
+                            className='rounded-full p-sm ml-auto hover:bg-primary-lighter'
+                        >
+                            <MdPersonAddAlt className='text-lg text-primary' />
+                        </button>
+                    </div>
+                </div>
+            </section>
+        </Protected>
+    );
+}
