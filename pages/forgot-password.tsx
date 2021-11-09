9059f35f723d908619065059358ae3c5f6dd98ce
Jonathan Tarnate <jstarnate16@gmail.com>
2021-11-09 17:13:46 +0800

    Applied requesting password reset and resetting password functionalities.

--- pages/forgot-password.tsx
+++ pages/forgot-password.tsx
@@ -1,29 +1,158 @@
+import { FormEvent, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { AxiosError } from 'axios';
+import clsx from 'clsx';
 import Public from 'components/Public';
 import InputField from 'components/utilities/InputField';
+import Radio from 'components/utilities/Radio';
+import axios from 'config/axios';
+
+interface AlertNotifcation {
+    status: number;
+    message: string;
+}
+
+interface ForgotPasswordFormDataError {
+    email: string[];
+    method: string[];
+}
+
+const fields = {
+    email: '',
+    method: 'email',
+};
 
 export default function ForgotPassword() {
+    const [alertNotification, setAlertNotification] =
+        useState<AlertNotifcation | null>(null);
+    const [loading, setLoading] = useState<boolean>(false);
+    const { register, watch, getValues, setError, clearErrors, formState } =
+        useForm({
+            defaultValues: fields,
+        });
+
+    const method = watch('method');
+
+    function processFormErrors(error: AxiosError) {
+        const { errors } = error.response?.data;
+        const keys = Object.keys(fields);
+
+        if (alertNotification) {
+            setAlertNotification(null);
+        }
+
+        keys.forEach(key => {
+            if (errors[key]) {
+                setError(key as keyof ForgotPasswordFormDataError, {
+                    type: 'manual',
+                    message: errors[key][0],
+                });
+            } else {
+                clearErrors(key as keyof ForgotPasswordFormDataError);
+            }
+        });
+    }
+
+    async function submit(event: FormEvent) {
+        event.preventDefault();
+
+        setLoading(true);
+
+        try {
+            const { data } = await axios().post(
+                '/forgot-password',
+                getValues(),
+            );
+
+            setLoading(false);
+            setAlertNotification(data);
+        } catch (error: AxiosError) {
+            const { status, data } = error.response;
+
+            setLoading(false);
+
+            if (status === 422) {
+                processFormErrors(error);
+            } else {
+                clearErrors();
+                setAlertNotification({
+                    status,
+                    message: data.message,
+                });
+            }
+        }
+    }
+
     return (
         <Public title='Forgot password - Sosyal.me'>
             <div className='py-md'>
                 <main className='max-w-[480px] m-auto rounded-md bg-skin-bg-contrast p-lg'>
-                    <div className='bg-primary-lighter rounded-md p-sm'>
-                        <p className='text-primary text-md m-0'>
-                            After successfully making a request, you should
-                            reset your password within <b>30 minutes</b>.
-                            Otherwise, send another request.
-                        </p>
-                    </div>
+                    {!!alertNotification && (
+                        <div
+                            className={clsx(
+                                'border rounded-md p-sm',
+                                alertNotification.status === 200
+                                    ? 'bg-primary-lighter text-primary border-primary'
+                                    : 'bg-danger-lighter text-danger border-danger',
+                            )}
+                        >
+                            <p className='text-md m-0'>
+                                {/* After successfully making a request, you should
+                                reset your password within <b>30 minutes</b>.
+                                Otherwise, send another request. */}
+                                {alertNotification.message}
+                            </p>
+                        </div>
+                    )}
 
-                    <form className='mt-lg'>
+                    <form className='mt-lg' onSubmit={submit}>
                         <InputField
                             id='email'
                             type='email'
                             label='Email address'
+                            error={formState.errors.email?.message}
+                            disabled={loading}
+                            {...register('email')}
                         />
+
+                        <section className='mt-lg'>
+                            <span className='block text-skin-text text-md font-bold'>
+                                Verification method
+                            </span>
+
+                            <div className='flex items-center mt-xs'>
+                                <Radio
+                                    containerClassName='flex items-center cursor-pointer'
+                                    id='email_verification'
+                                    label='Email'
+                                    value='email'
+                                    checked={method === 'email'}
+                                    disabled={loading}
+                                    {...register('method')}
+                                />
+
+                                <Radio
+                                    containerClassName='flex items-center cursor-pointer ml-xl'
+                                    id='sms_verification'
+                                    label='SMS'
+                                    value='sms'
+                                    checked={method === 'sms'}
+                                    disabled={loading}
+                                    {...register('method')}
+                                />
+                            </div>
+
+                            {!!formState.errors.method && (
+                                <p className='text-danger text-sm mt-xs mb-0'>
+                                    {formState.errors.method.message}
+                                </p>
+                            )}
+                        </section>
+
                         <button
-                            type='button'
+                            type='submit'
                             className='btn-primary w-full text-md mt-lg'
-                            disabled
+                            disabled={loading}
                         >
                             Send request
                         </button>
--- pages/reset-password/[token].tsx
+++ pages/reset-password/[token].tsx
@@ -1,7 +1,63 @@
+import { GetServerSideProps } from 'next';
+import { useRouter } from 'next/router';
+import { FormEvent, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { AxiosError } from 'axios';
+import Cookies from 'js-cookie';
 import Public from 'components/Public';
 import InputField from 'components/utilities/InputField';
+import axios from 'config/axios';
 
-export default function ForgotPassword() {
+interface FormDataError {
+    email: string[];
+    password: string[];
+    password_confirmation: string[];
+}
+
+const fields = {
+    email: '',
+    password: '',
+    password_confirmation: '',
+};
+
+export default function ResetPassword({ token }: { token: string }) {
+    const [loading, setLoading] = useState<boolean>(false);
+    const { replace } = useRouter();
+    const { register, getValues, setError, clearErrors, formState } = useForm({
+        defaultValues: { ...fields, token },
+    });
+
+    async function submit(event: FormEvent) {
+        event.preventDefault();
+
+        setLoading(true);
+
+        try {
+            const { data } = await axios().put('/reset-password', getValues());
+
+            Cookies.set('user', JSON.stringify(data.user));
+            Cookies.set('token', data.token);
+
+            replace('/home');
+        } catch (error) {
+            const { errors } = (error as AxiosError).response?.data;
+            const keys = Object.keys(fields);
+
+            setLoading(false);
+
+            keys.forEach(key => {
+                if (errors[key]) {
+                    setError(key as keyof FormDataError, {
+                        type: 'manual',
+                        message: errors[key][0],
+                    });
+                } else {
+                    clearErrors(key as keyof FormDataError);
+                }
+            });
+        }
+    }
+
     return (
         <Public title='Reset password - Sosyal.me'>
             <div className='py-md'>
@@ -10,29 +66,40 @@ export default function ForgotPassword() {
                         Reset your password
                     </h1>
 
-                    <form className='mt-lg'>
+                    <form className='mt-lg' onSubmit={submit}>
                         <InputField
                             id='email'
                             type='email'
                             label='Email address'
+                            error={formState.errors.email?.message}
+                            disabled={loading}
+                            {...register('email')}
                         />
                         <InputField
                             containerClassName='mt-lg'
                             id='password'
                             type='password'
                             label='New password'
+                            error={formState.errors.password?.message}
+                            disabled={loading}
+                            {...register('password')}
                         />
                         <InputField
                             containerClassName='mt-lg'
                             id='password_confirmation'
                             type='password'
                             label='Confirm new password'
+                            disabled={loading}
+                            error={
+                                formState.errors.password_confirmation?.message
+                            }
+                            {...register('password_confirmation')}
                         />
 
                         <button
-                            type='button'
+                            type='submit'
                             className='btn-primary w-full text-md mt-lg'
-                            disabled
+                            disabled={loading}
                         >
                             Reset my password
                         </button>
@@ -42,3 +109,49 @@ export default function ForgotPassword() {
         </Public>
     );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({
+    params,
+    req,
+}) => {
+    if (!req.cookies || !req.cookies.token) {
+        try {
+            await axios().get(`/reset-password/${params?.token}`);
+
+            return {
+                props: {
+                    token: params?.token,
+                },
+            };
+        } catch (err) {
+            return {
+                notFound: true,
+            };
+        }
+    }
+
+    try {
+        await axios(req.cookies.token).get(`${process.env.APP_URL}/private`);
+
+        return {
+            redirect: {
+                destination: '/home',
+                permanent: false,
+            },
+        };
+    } catch (e) {
+        try {
+            await axios().get(`/reset-password/${params?.token}`);
+
+            return {
+                props: {
+                    token: params?.token,
+                },
+            };
+        } catch (err) {
+            return {
+                notFound: true,
+            };
+        }
+    }
+};
