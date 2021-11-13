d2abf5d214d8392136bcf54959af933066fdcdca
Jonathan Tarnate <jstarnate16@gmail.com>
2021-11-13 22:57:38 +0800

    Refactored default wrapper and added closeEvent prop.

--- components/layouts/modal/EditPost.tsx
+++ components/layouts/modal/EditPost.tsx
@@ -1,7 +1,6 @@
 import { useEffect } from 'react';
 import { useQueryClient, useQuery, InfiniteData } from 'react-query';
 import { MdAccountCircle, MdClose } from 'react-icons/md';
-import { Dialog } from '@headlessui/react';
 import PostBox from 'components/chunks/PostBox';
 import type { Post } from 'types/post';
 import type { PostPage } from 'types/page';
@@ -42,59 +41,40 @@ export default function EditPostModal() {
     }, []);
 
     return (
-        <Modal>
-            <Dialog
-                as='div'
-                className='modal-wrapper overflow-y-auto'
-                open
-                onClose={closeModal}
-            >
-                <div className='w-[720px] px-4 text-center'>
-                    {/* This element is to trick the browser into centering the modal contents. */}
-                    <span
-                        className='inline-block h-screen align-middle'
-                        aria-hidden='true'
-                    >
-                        &#8203;
-                    </span>
+        <Modal closeEvent={closeModal}>
+            <header className='flex items-center py-sm px-md'>
+                <MdAccountCircle
+                    className='text-skin-text'
+                    size={35}
+                    viewBox='2 2 20 20'
+                />
 
-                    <div className='inline-block w-full overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl bg-skin-bg'>
-                        <header className='flex items-center py-sm px-md'>
-                            <MdAccountCircle
-                                className='text-skin-text'
-                                size={35}
-                                viewBox='2 2 20 20'
-                            />
+                <h1 className='text-md text-skin-text font-bold ml-sm'>
+                    Edit post
+                </h1>
 
-                            <h1 className='text-md text-skin-text font-bold ml-sm'>
-                                Edit post
-                            </h1>
+                <button
+                    type='button'
+                    className='block rounded-full p-xs ml-auto hover:bg-skin-bg-contrast'
+                    onClick={closeModal}
+                >
+                    <MdClose
+                        className='text-skin-text'
+                        size={20}
+                        viewBox='2 2 20 20'
+                    />
+                </button>
+            </header>
 
-                            <button
-                                type='button'
-                                className='block rounded-full p-xs ml-auto hover:bg-skin-bg-contrast'
-                                onClick={closeModal}
-                            >
-                                <MdClose
-                                    className='text-skin-text'
-                                    size={20}
-                                    viewBox='2 2 20 20'
-                                />
-                            </button>
-                        </header>
-
-                        <PostBox
-                            placeholder="What's on your mind?"
-                            buttonLabel='Edit post'
-                            rows={5}
-                            value={selectedPost?.body || ''}
-                            apiUrl={`/api/posts/${selectedPost?.slug}`}
-                            apiMethod='put'
-                            onSuccessEvent={onSuccessEvent}
-                        />
-                    </div>
-                </div>
-            </Dialog>
+            <PostBox
+                placeholder="What's on your mind?"
+                buttonLabel='Edit post'
+                rows={5}
+                value={selectedPost?.body || ''}
+                apiUrl={`/api/posts/${selectedPost?.slug}`}
+                apiMethod='put'
+                onSuccessEvent={onSuccessEvent}
+            />
         </Modal>
     );
 }
--- components/layouts/modal/index.tsx
+++ components/layouts/modal/index.tsx
@@ -1,7 +1,13 @@
 import { useEffect, useState, useRef, ReactNode } from 'react';
 import { createPortal } from 'react-dom';
+import { Dialog } from '@headlessui/react';
 
-export default function Modal({ children }: { children: ReactNode }) {
+interface Props {
+    closeEvent(): void;
+    children: ReactNode;
+}
+
+export default function Modal({ closeEvent, children }: Props) {
     const [mounted, setMounted] = useState<boolean>(false);
     const ref = useRef<Element | null>(null);
 
@@ -15,5 +21,31 @@ export default function Modal({ children }: { children: ReactNode }) {
         };
     }, []);
 
-    return mounted ? createPortal(children, ref.current as Element) : null;
+    if (!mounted) {
+        return null;
+    }
+
+    return createPortal(
+        <Dialog
+            as='div'
+            className='modal-wrapper overflow-y-auto'
+            open
+            onClose={closeEvent}
+        >
+            <div className='w-[720px] px-4 text-center'>
+                {/* This element is to trick the browser into centering the modal contents. */}
+                <span
+                    className='inline-block h-screen align-middle'
+                    aria-hidden='true'
+                >
+                    &#8203;
+                </span>
+
+                <div className='inline-block w-full overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl bg-skin-bg'>
+                    {children}
+                </div>
+            </div>
+        </Dialog>,
+        ref.current as Element,
+    );
 }
