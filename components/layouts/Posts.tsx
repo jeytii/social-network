debf8863479e8f678241b8de4b56d859bf813a35
Jonathan Tarnate <jstarnate16@gmail.com>
2021-11-14 01:46:12 +0800

    Added functionality for sorting posts based on query parameter.

--- components/layouts/Posts.tsx
+++ components/layouts/Posts.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useEffect } from 'react';
+import { ChangeEvent, Fragment, useEffect } from 'react';
 import { useInfiniteQuery } from 'react-query';
 import Cookies from 'js-cookie';
 import Post from 'components/chunks/Post';
@@ -6,33 +6,38 @@ import Select from 'components/utilities/Select';
 import Spinner from 'components/vectors/Spinner';
 import axios from 'config/axios';
 import type { PostPage } from 'types/page';
+import { useRouter } from 'next/router';
 
 const items = [
     { label: 'Timestamp', value: 'created_at' },
     { label: 'Number of likes', value: 'likes' },
 ];
 
-async function getPosts() {
-    const { data } = await axios(Cookies.get('token')).get('/api/posts');
+const getPosts = (sort: string | string[] | undefined) => async () => {
+    const { data } = await axios(Cookies.get('token')).get('/api/posts', {
+        params: { sort },
+    });
 
     return data;
-}
+};
 
 export default function Posts() {
-    const { data, isLoading, isSuccess, refetch } = useInfiniteQuery<PostPage>(
-        'posts',
-        getPosts,
-        {
+    const { asPath, query, replace } = useRouter();
+    const { data, isLoading, isSuccess, isRefetching, refetch } =
+        useInfiniteQuery<PostPage>('posts', getPosts(query.sort), {
             enabled: false,
             getNextPageParam: last => (last.has_more ? last.next_offset : null),
-        },
-    );
+        });
+
+    const changeSortType = (event: ChangeEvent<HTMLSelectElement>) => {
+        replace(`/home?sort=${event.target.value}`);
+    };
 
     useEffect(() => {
         refetch();
-    }, []);
+    }, [asPath]);
 
-    if (isLoading) {
+    if (isRefetching || isLoading) {
         return <Spinner className='p-lg' />;
     }
 
@@ -53,7 +58,8 @@ export default function Posts() {
                 <Select
                     className='text-skin-text-light text-sm bg-skin-bg ml-sm cursor-pointer'
                     items={items}
-                    defaultValue='created_at'
+                    value={query.sort || 'created_at'}
+                    onChange={changeSortType}
                 />
             </div>
 
