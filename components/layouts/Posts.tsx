1490e6319bd68a5e04ec53243a2fc8b124871078
Jonathan Tarnate <jstarnate16@gmail.com>
2021-11-16 14:58:20 +0800

    Fixed data-fetching on initial render.

--- components/layouts/Posts.tsx
+++ components/layouts/Posts.tsx
@@ -1,43 +1,45 @@
-import { ChangeEvent, Fragment, useEffect } from 'react';
-import { useInfiniteQuery } from 'react-query';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import { ChangeEvent, Fragment } from 'react';
+import { QueryFunctionContext, useInfiniteQuery } from 'react-query';
 import Cookies from 'js-cookie';
 import Post from 'components/chunks/post';
 import Select from 'components/utilities/Select';
 import Spinner from 'components/vectors/Spinner';
 import axios from 'config/axios';
 import type { PostPage } from 'types/page';
-import { useRouter } from 'next/router';
 
 const items = [
     { label: 'Timestamp', value: 'created_at' },
     { label: 'Number of likes', value: 'likes' },
 ];
 
-const getPosts = (sort: string | string[] | undefined) => async () => {
+const getPosts = (sort: string) => async (ctx: QueryFunctionContext) => {
     const { data } = await axios(Cookies.get('token')).get('/api/posts', {
-        params: { sort },
+        params: {
+            page: ctx.pageParam || 1,
+            sort,
+        },
     });
 
     return data;
 };
 
 export default function Posts() {
-    const { asPath, query, replace } = useRouter();
-    const { data, isLoading, isSuccess, isRefetching, refetch } =
-        useInfiniteQuery<PostPage>('posts', getPosts(query.sort), {
-            enabled: false,
+    const { query, replace } = useRouter();
+    const { data, isLoading, isSuccess } = useInfiniteQuery<PostPage>(
+        'posts',
+        getPosts(query.sort as string),
+        {
             getNextPageParam: last => (last.has_more ? last.next_offset : null),
-        });
+        },
+    );
 
     const changeSortType = (event: ChangeEvent<HTMLSelectElement>) => {
         replace(`/home?sort=${event.target.value}`);
     };
 
-    useEffect(() => {
-        refetch();
-    }, [asPath]);
-
-    if (isRefetching || isLoading) {
+    if (isLoading) {
         return <Spinner className='p-lg' />;
     }
 
@@ -67,7 +69,12 @@ export default function Posts() {
                 {data?.pages?.map(page => (
                     <Fragment key={page.next_offset}>
                         {page.items.map(post => (
-                            <Post key={post.slug} className='mt-lg' {...post} />
+                            <Link key={post.slug} href={`/post/${post.slug}`}>
+                                <Post
+                                    className='cursor-pointer mt-lg'
+                                    {...post}
+                                />
+                            </Link>
                         ))}
                     </Fragment>
                 ))}
