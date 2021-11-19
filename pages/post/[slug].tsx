36ef09d82206812cbd07efb9612b3dd78561fd56
Jonathan Tarnate <jstarnate16@gmail.com>
2021-11-19 23:50:44 +0800

    Fixed error handling.

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -2,11 +2,11 @@ import { GetServerSideProps } from 'next';
 import { useQuery, QueryClient, useQueryClient } from 'react-query';
 import Cookies from 'js-cookie';
 import Post from 'components/chunks/post';
+import Comments from 'components/layouts/Comments';
 import CommentBox from 'components/chunks/CommentBox';
 import Spinner from 'components/vectors/Spinner';
 import axios from 'config/axios';
 import type { Post as PostType } from 'types/post';
-import Comments from 'components/layouts/Comments';
 
 const getPost = (queryClient: QueryClient, slug: string) => async () => {
     const post = queryClient.getQueryData<PostType>(['post', slug]);
@@ -15,27 +15,41 @@ const getPost = (queryClient: QueryClient, slug: string) => async () => {
         return post;
     }
 
-    const { data } = await axios(Cookies.get('token')).get(
-        `/api/posts/${slug}`,
-    );
+    try {
+        const { data } = await axios(Cookies.get('token')).get(
+            `/api/posts/${slug}`,
+        );
 
-    return data.post;
+        return data.post;
+    } catch (error) {
+        if (error.response.status === 404) {
+            throw new Error('Post does not exist.');
+        }
+
+        throw new Error('Something went wrong.');
+    }
 };
 
 export default function ViewPost({ slug }: { slug: string }) {
     const queryClient = useQueryClient();
-
-    const { data, isLoading, isError } = useQuery<PostType>(
+    const { data, isLoading, isError, error } = useQuery<PostType, Error>(
         ['post', slug],
         getPost(queryClient, slug),
+        {
+            cacheTime: 1000 * 60 * 2,
+        },
     );
 
     if (isLoading) {
         return <Spinner className='p-lg' />;
     }
 
-    if (isError) {
-        return <h1>Error</h1>;
+    if (isError || !data) {
+        return (
+            <h1 className='text-md text-skin-text-light text-center p-lg'>
+                {error?.message}
+            </h1>
+        );
     }
 
     return (
