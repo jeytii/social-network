dcf65cd7f6a9e2d70b63c59f0df7c75658c51a89
Jonathan Tarnate <jstarnate16@gmail.com>
2021-11-23 22:43:57 +0800

    Wrapped the component in forwardRef().

--- components/utilities/Select.tsx
+++ components/utilities/Select.tsx
@@ -1,4 +1,4 @@
-import { SelectHTMLAttributes } from 'react';
+import { ForwardedRef, forwardRef, SelectHTMLAttributes } from 'react';
 
 interface Props extends SelectHTMLAttributes<HTMLSelectElement> {
     items: {
@@ -7,18 +7,22 @@ interface Props extends SelectHTMLAttributes<HTMLSelectElement> {
     }[];
 }
 
-export default function Select({ items, ...props }: Props) {
-    return (
-        <select {...props}>
-            {!!props.placeholder && (
-                <option className='bg-skin-bg'>{props.placeholder}</option>
-            )}
+const Select = forwardRef(
+    ({ items, ...props }: Props, ref: ForwardedRef<HTMLSelectElement>) => {
+        return (
+            <select ref={ref} {...props}>
+                {!!props.placeholder && (
+                    <option className='bg-skin-bg'>{props.placeholder}</option>
+                )}
 
-            {items.map(({ label, value }) => (
-                <option key={value} className='bg-skin-bg' value={value}>
-                    {label}
-                </option>
-            ))}
-        </select>
-    );
-}
+                {items.map(({ label, value }) => (
+                    <option key={value} className='bg-skin-bg' value={value}>
+                        {label}
+                    </option>
+                ))}
+            </select>
+        );
+    },
+);
+
+export default Select;
