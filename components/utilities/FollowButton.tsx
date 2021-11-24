9fa9a1dc98402713441d1ec17333e251891beee8
Jonathan Tarnate <jstarnate16@gmail.com>
2021-11-25 02:58:13 +0800

    Created a separate component for profile section's follow button.

---
+++ components/utilities/FollowButton.tsx
@@ -0,0 +1,59 @@
+import { useState, MouseEvent } from 'react';
+import clsx from 'clsx';
+import useDebounceClick from 'hooks/useDebounceClick';
+import { axiosClient } from 'config/axios';
+
+interface Props {
+    userSlug: string;
+    condition: boolean;
+}
+
+export default function FollowButton({ userSlug, condition }: Props) {
+    const [followed, setFollowed] = useState<boolean>(condition);
+    const [debounce, mutatePrevious] = useDebounceClick(
+        followed,
+        follow,
+        unfollow,
+    );
+
+    async function follow() {
+        try {
+            await axiosClient().post(`/api/users/${userSlug}/follow`);
+
+            mutatePrevious(true);
+        } catch (e) {
+            setFollowed(false);
+        }
+    }
+
+    async function unfollow() {
+        try {
+            await axiosClient().delete(`/api/users/${userSlug}/unfollow`);
+
+            mutatePrevious(false);
+        } catch (e) {
+            setFollowed(true);
+        }
+    }
+
+    function toggleFollow(event: MouseEvent<HTMLButtonElement>) {
+        event.preventDefault();
+
+        setFollowed(current => !current);
+
+        debounce();
+    }
+
+    return (
+        <button
+            className={clsx(
+                'button button-primary-outlined text-sm rounded-full ml-auto sm:mt-sm',
+                followed ? 'button-danger-outlined' : 'button-primary-outlined',
+            )}
+            type='button'
+            onClick={toggleFollow}
+        >
+            {followed ? 'Unfollow' : 'Follow'}
+        </button>
+    );
+}
