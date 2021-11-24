9f38aed34bd5679e856c0fb25218699ff5e1bb80
Jonathan Tarnate <jstarnate16@gmail.com>
2021-11-24 16:32:32 +0800

    Added functionality for changing the phone number.

--- pages/settings/phone-number.tsx
+++ pages/settings/phone-number.tsx
@@ -1,39 +1,157 @@
-import Protected from 'components/Protected';
+import { GetServerSideProps } from 'next';
+import { FormEvent, useState } from 'react';
+import { useMutation } from 'react-query';
+import { useForm } from 'react-hook-form';
+import { AxiosError } from 'axios';
 import InputField from 'components/utilities/InputField';
+import { axiosServer } from 'config/axios';
 
-export default function ChangePhoneNumber() {
+interface Variables {
+    url: string;
+    data: {
+        phone_number: string;
+        password: string;
+    };
+}
+
+export default function ChangePhoneNumber({
+    phone_number,
+}: {
+    phone_number: string;
+}) {
+    const [errorAlert, setErrorAlert] = useState<string | null>(null);
+    const { mutate, isLoading } = useMutation<unknown, AxiosError, Variables>(
+        'update',
+        { onSuccess, onError },
+    );
+    const { register, getValues, watch, setError, clearErrors, formState } =
+        useForm({
+            defaultValues: { phone_number, password: '' },
+        });
+
+    const [phoneNumber, password] = watch(['phone_number', 'password']);
+
+    function onError(error: AxiosError) {
+        if (error.response?.status === 422) {
+            const { errors } = error.response.data;
+            const keys: ['phone_number', 'password'] = [
+                'phone_number',
+                'password',
+            ];
+
+            if (errorAlert) {
+                setErrorAlert(null);
+            }
+
+            keys.forEach(key => {
+                if (errors[key]) {
+                    setError(key, {
+                        type: 'manual',
+                        message: errors[key][0],
+                    });
+                } else {
+                    clearErrors(key);
+                }
+            });
+        } else {
+            clearErrors();
+            setErrorAlert(error.response?.data.message);
+        }
+    }
+
+    function onSuccess() {
+        if (errorAlert) {
+            setErrorAlert(null);
+        }
+
+        clearErrors();
+
+        window.location.href = '/settings';
+    }
+
+    function submit(event: FormEvent) {
+        event.preventDefault();
+
+        mutate({
+            url: '/api/settings/change/phone',
+            data: getValues(),
+        });
+    }
+
     return (
-        <Protected title='Change phone number'>
-            <section className='p-lg'>
-                <div className='flex items-center bg-primary-lighter text-md text-primary p-md border-primary rounded-md'>
-                    You can only change your phone number 3 times within 30
-                    days.
-                </div>
+        <section className='p-lg'>
+            {errorAlert && errorAlert.length && (
+                <p className='bg-danger-light text-md text-danger rounded-md p-sm mb-lg'>
+                    {errorAlert}
+                </p>
+            )}
 
-                <form className='mt-sm'>
-                    <InputField
-                        className='text-md'
-                        id='phone-number'
-                        type='text'
-                        label='Phone number'
-                    />
+            <form onSubmit={submit}>
+                <InputField
+                    className='text-md'
+                    id='phone'
+                    type='tel'
+                    label='Phone number'
+                    disabled={isLoading}
+                    error={formState.errors.phone_number?.message}
+                    autoFocus
+                    {...register('phone_number')}
+                />
 
-                    <InputField
-                        containerClassName='mt-lg'
-                        className='text-md'
-                        id='password'
-                        type='password'
-                        label='Password'
-                    />
+                <InputField
+                    containerClassName='mt-lg'
+                    className='text-md'
+                    id='password'
+                    type='password'
+                    label='Password'
+                    disabled={isLoading}
+                    error={formState.errors.password?.message}
+                    {...register('password')}
+                />
 
-                    <button
-                        className='button button-primary w-full mt-lg'
-                        type='button'
-                    >
-                        Update my phone number
-                    </button>
-                </form>
-            </section>
-        </Protected>
+                <button
+                    className='button button-primary w-full py-sm mt-lg'
+                    type='submit'
+                    disabled={
+                        isLoading ||
+                        !phoneNumber.length ||
+                        !password.length ||
+                        (!!phoneNumber.length && phoneNumber === phone_number)
+                    }
+                >
+                    Update my phone number
+                </button>
+            </form>
+        </section>
     );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+    if (!req.cookies || !req.cookies.token) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false,
+            },
+        };
+    }
+
+    try {
+        const { data } = await axiosServer(req.cookies.token).get('/private');
+
+        return {
+            props: {
+                title: 'Change phone number',
+                isPrivate: true,
+                phone_number: data.user.phone_number,
+            },
+        };
+    } catch (e) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false,
+            },
+        };
+    }
+};
