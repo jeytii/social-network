7d2df4a165aa9a6b5c55140e8aa2d5387ccc3302
Jonathan Tarnate <jstarnate16@gmail.com>
2021-12-08 22:08:55 +0800

    Added http call on upload event.

--- components/layouts/profile/ImageUploader.tsx
+++ components/layouts/profile/ImageUploader.tsx
@@ -1,46 +1,81 @@
 import Image from 'next/image';
-import { useState } from 'react';
-import { MdAccountCircle, MdCameraAlt } from 'react-icons/md';
+import { ChangeEvent } from 'react';
+import { MdAccountCircle, MdUpload, MdOutlineRefresh } from 'react-icons/md';
+import clsx from 'clsx';
 import Spinner from 'components/vectors/Spinner';
 
-export default function ImageUploader() {
-    const [loading, setLoading] = useState<boolean>(false);
+interface Props {
+    src: string | null;
+    initialSrc: string | null;
+    loading: boolean;
+    hasError: boolean;
+    onChange(event: ChangeEvent<HTMLInputElement>): void;
+    onReset(): void;
+}
 
+export default function ImageUploader({
+    src,
+    initialSrc,
+    loading,
+    hasError,
+    onChange,
+    onReset,
+}: Props) {
     return (
         <section>
             {loading ? (
-                <Spinner className='w-[100px] h-[100px] rounded-full bg-skin-bg-contrast m-auto' />
+                <Spinner className='flex items-center justify-center w-[100px] h-[100px] rounded-full bg-skin-bg-contrast m-auto' />
             ) : (
-                <div className='relative block w-[100px] rounded-full overflow-hidden m-auto'>
-                    <MdAccountCircle
-                        className='text-skin-text-light block w-full h-full'
-                        viewBox='2 2 20 20'
-                    />
-
-                    {/* <Image
-                        className="block w-full h-full"
-                        src="https://avatarfiles.alphacoders.com/867/thumb-1920-86706.jpg"
-                        width="100%"
-                        height="100%"
-                        layout="responsive"
-                    /> */}
-
-                    <label
-                        className='absolute w-full bottom-[0px] py-sm cursor-pointer'
-                        style={{ background: 'rgba(0, 0, 0, 0.5)' }}
-                        htmlFor='upload'
-                    >
-                        <input className='hidden' type='file' id='upload' />
-                        <MdCameraAlt
-                            className='block m-auto'
-                            size={20}
-                            fill='#fff'
+                <div className='block w-[100px] rounded-full m-auto'>
+                    {src ? (
+                        <Image
+                            className='block w-full h-full'
+                            src={src}
+                            width='100%'
+                            height='100%'
+                            layout='responsive'
                         />
-                    </label>
+                    ) : (
+                        <MdAccountCircle
+                            className='text-skin-text-light block w-full h-full'
+                            viewBox='2 2 20 20'
+                        />
+                    )}
                 </div>
             )}
 
-            <p className='text-skin-text-light text-sm text-center mt-xs'>
+            <div className='text-center mt-sm'>
+                <label
+                    htmlFor='upload'
+                    className='button button-primary inline-flex items-center font-normal text-sm'
+                >
+                    <MdUpload />
+                    <span className='ml-xs'>Upload</span>
+                    <input
+                        className='hidden'
+                        type='file'
+                        id='upload'
+                        onChange={onChange}
+                    />
+                </label>
+
+                <button
+                    className='button button-default inline-flex items-center font-normal text-sm ml-sm'
+                    type='button'
+                    disabled={src === initialSrc}
+                    onClick={onReset}
+                >
+                    <MdOutlineRefresh />
+                    <span className='ml-xs'>Reset</span>
+                </button>
+            </div>
+
+            <p
+                className={clsx(
+                    'text-sm text-center mt-sm',
+                    hasError ? 'text-danger' : 'text-skin-text-light',
+                )}
+            >
                 Must be between <b>200x200</b> and <b>800x800</b>.
             </p>
         </section>
