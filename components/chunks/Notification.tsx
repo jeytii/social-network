d78d3426456603184f231f2893454d5f4eba9c76
Jonathan Tarnate <jstarnate16@gmail.com>
2021-12-10 02:04:02 +0800

    Created an independent Notification component.

---
+++ components/chunks/Notification.tsx
@@ -0,0 +1,55 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import { forwardRef, Ref } from 'react';
+import { MdAccountCircle } from 'react-icons/md';
+import clsx from 'clsx';
+import type { Notification as NotificationType } from 'types/notification';
+
+interface Props extends NotificationType {
+    className: string;
+}
+
+const Notification = forwardRef(
+    ({ className, ...props }: Props, ref: Ref<HTMLElement>) => {
+        const { name, image_url } = props.user;
+
+        return (
+            <Link href={props.path}>
+                <figure
+                    ref={ref}
+                    className={clsx(
+                        'flex items-center text-skin-text border p-sm rounded-md cursor-pointer',
+                        {
+                            'bg-skin-bg-contrast-light': props.is_read,
+                            'border-skin-bg-contrast': props.is_read,
+                            'bg-primary-lighter': !props.is_read,
+                            'border-primary': !props.is_read,
+                        },
+                        className,
+                    )}
+                >
+                    {image_url ? (
+                        <Image
+                            className='block'
+                            src={image_url}
+                            width={50}
+                            height={50}
+                        />
+                    ) : (
+                        <MdAccountCircle
+                            className='block text-skin-text-light'
+                            size={50}
+                            viewBox='2 2 20 20'
+                        />
+                    )}
+
+                    <p className='text-md ml-sm sm:text-sm'>
+                        <b>{name}</b> <span>{props.message}</span>
+                    </p>
+                </figure>
+            </Link>
+        );
+    },
+);
+
+export default Notification;
