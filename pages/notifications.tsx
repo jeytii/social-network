8b2765e5811f4842d099c6325839705e0086ac73
Jonathan Tarnate <jstarnate16@gmail.com>
2021-12-10 02:03:21 +0800

    Populate the notifications.

--- pages/notifications.tsx
+++ pages/notifications.tsx
@@ -1,69 +1,96 @@
-import Link from 'next/link';
-import { MdAccountCircle, MdOutlineChecklist } from 'react-icons/md';
-import Protected from 'components/Protected';
+import { GetServerSideProps } from 'next';
+import { MdOutlineChecklist } from 'react-icons/md';
+import Notification from 'components/chunks/Notification';
+import Spinner from 'components/vectors/Spinner';
+import useInfiniteScroll from 'hooks/useInfiniteScroll';
+import { axiosServer } from 'config/axios';
+import { NotificationPage } from 'types/page';
+import { Notification as NotificationType } from 'types/notification';
+import clsx from 'clsx';
 
 export default function Notifications() {
-    return (
-        <Protected title='Notifications - Sosyal.me'>
-            <section className='px-lg pt-sm pb-lg sm:px-md'>
-                <div className='flex items-center'>
-                    <h1 className='text-skin-text font-semibold'>
-                        Notifications
-                    </h1>
+    const { data, ref, isLoading, isFetchingNextPage, isSuccess } =
+        useInfiniteScroll<NotificationPage, NotificationType>({
+            queryKey: 'notifications',
+            meta: {
+                url: '/api/notifications',
+            },
+            cacheTime: Infinity,
+            select: ({ pageParams, pages }) => ({
+                pageParams,
+                pages: pages.flatMap(page => [...page.items]),
+            }),
+        });
 
-                    <button
-                        className='p-sm rounded-full ml-auto hover:bg-primary-lighter'
-                        type='button'
-                        title='Mark all as read'
-                    >
-                        <MdOutlineChecklist className='text-primary text-xl' />
-                    </button>
-                </div>
+    if (isLoading) {
+        return <Spinner className='p-lg' />;
+    }
 
-                <div className='mt-sm'>
-                    <div>
-                        <Link href='/'>
-                            <span className='flex items-center bg-skin-bg-contrast-light text-skin-text border border-skin-bg-contrast p-sm rounded-md cursor-pointer'>
-                                <MdAccountCircle
-                                    className='block text-skin-text-light'
-                                    size={60}
-                                />
-                                <span className='text-md ml-sm sm:text-sm'>
-                                    <b>John Doe</b> <span>followed you.</span>
-                                </span>
-                            </span>
-                        </Link>
-                    </div>
-
-                    <div className='mt-lg'>
-                        <Link href='/'>
-                            <span className='flex items-center bg-primary-lighter text-skin-text border border-primary p-sm rounded-md cursor-pointer'>
-                                <MdAccountCircle
-                                    className='block text-skin-text-light'
-                                    size={60}
-                                />
-                                <p className='text-md ml-sm sm:text-sm'>
-                                    <b>John Doe</b> <span>followed you.</span>
-                                </p>
-                            </span>
-                        </Link>
-                    </div>
-
-                    <div className='mt-lg'>
-                        <Link href='/'>
-                            <span className='flex items-center bg-skin-bg-contrast-light text-skin-text border border-skin-bg-contrast p-sm rounded-md cursor-pointer'>
-                                <MdAccountCircle
-                                    className='block text-skin-text-light'
-                                    size={60}
-                                />
-                                <span className='text-md ml-sm sm:text-sm'>
-                                    <b>John Doe</b> <span>followed you.</span>
-                                </span>
-                            </span>
-                        </Link>
-                    </div>
-                </div>
+    if (isSuccess && !data?.pages.length) {
+        return (
+            <section className='p-lg'>
+                <h1 className='text-md font-bold text-skin-text-light opacity-50 text-center'>
+                    No notification to show.
+                </h1>
             </section>
-        </Protected>
+        );
+    }
+
+    return (
+        <section className='px-lg pt-sm pb-lg sm:px-md'>
+            <div className='flex items-center'>
+                <h1 className='text-skin-text font-semibold'>Notifications</h1>
+
+                <button
+                    className='p-sm rounded-full ml-auto hover:bg-primary-lighter'
+                    type='button'
+                    title='Mark all as read'
+                >
+                    <MdOutlineChecklist className='text-primary text-xl' />
+                </button>
+            </div>
+
+            <div className='mt-sm'>
+                {data?.pages.map((notification, index) => (
+                    <Notification
+                        key={notification.slug}
+                        ref={index === data.pages.length - 1 ? ref : null}
+                        className={clsx(!!index && 'mt-lg')}
+                        {...notification}
+                    />
+                ))}
+            </div>
+
+            {isFetchingNextPage && <Spinner className='mt-lg' />}
+        </section>
     );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+    if (!req.cookies || !req.cookies.token) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false,
+            },
+        };
+    }
+
+    try {
+        await axiosServer(req.cookies.token).get('/private');
+
+        return {
+            props: {
+                title: 'Notifications',
+                isPrivate: true,
+            },
+        };
+    } catch (e) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false,
+            },
+        };
+    }
+};
