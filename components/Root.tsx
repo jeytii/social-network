816492e46694942d6bc159fc4da8052f03557c60
Jonathan Tarnate <jstarnate16@gmail.com>
2021-12-23 18:18:19 +0800

    Added progress bar for loading pages.

--- components/Root.tsx
+++ components/Root.tsx
@@ -1,4 +1,5 @@
-import { ReactNode } from 'react';
+import { ReactNode, useEffect, useRef } from 'react';
+import { useRouter } from 'next/router';
 import Public from './Public';
 import Protected from './Protected';
 
@@ -10,9 +11,40 @@ interface Props {
 }
 
 export default function Root({ isPrivate, children, ...props }: Props) {
-    if (!isPrivate) {
-        return <Public {...props}>{children}</Public>;
-    }
+    const bar = useRef<HTMLDivElement | null>(null);
+    const { events } = useRouter();
 
-    return <Protected {...props}>{children}</Protected>;
+    useEffect(() => {
+        events.on('routeChangeStart', () => {
+            bar.current?.classList.add('transition-[width]', 'duration-500');
+            bar.current?.classList.replace('w-[0px]', 'w-[30%]');
+        });
+
+        events.on('routeChangeComplete', () => {
+            bar.current?.classList.replace('w-[30%]', 'w-full');
+
+            setTimeout(() => {
+                bar.current?.classList.remove(
+                    'transition-[width]',
+                    'duration-500',
+                );
+                bar.current?.classList.replace('w-full', 'w-[0px]');
+            }, 500);
+        });
+    }, []);
+
+    return (
+        <>
+            <div
+                ref={bar}
+                className='fixed top-[0px] left-[0px] w-[0px] h-[3px] bg-primary z-50'
+            />
+
+            {isPrivate ? (
+                <Protected {...props}>{children}</Protected>
+            ) : (
+                <Public {...props}>{children}</Public>
+            )}
+        </>
+    );
 }
