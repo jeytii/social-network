6548d21ee878bb71daa286ee74963ca1c1cb26a0
Jonathan Tarnate <jstarnate16@gmail.com>
2021-12-29 22:49:43 +0800

    Added a click event that sets the is_read attribute into true.

--- components/chunks/Notification.tsx
+++ components/chunks/Notification.tsx
@@ -1,57 +1,83 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import { forwardRef, Ref } from 'react';
+import { InfiniteData, useQueryClient } from 'react-query';
 import { MdAccountCircle } from 'react-icons/md';
 import clsx from 'clsx';
+import { axiosClient } from 'config/axios';
+import type { NotificationPage } from 'types/page';
 import type { Notification as NotificationType } from 'types/notification';
 
 interface Props extends NotificationType {
     className: string;
 }
 
-const Notification = forwardRef(
-    ({ className, ...props }: Props, ref: Ref<HTMLElement>) => {
-        const { name, image_url } = props.user;
+function Notification(
+    { className, slug, message, is_read, url, user }: Props,
+    ref: Ref<HTMLElement>,
+) {
+    const queryClient = useQueryClient();
 
-        return (
-            <Link href={props.url}>
-                <figure
-                    ref={ref}
-                    className={clsx(
-                        'flex items-center p-md rounded cursor-pointer',
-                        {
-                            'bg-primary-transparent': props.is_read,
-                            'text-skin-secondary': props.is_read,
-                            'bg-skin-main': !props.is_read,
-                            'text-skin-primary': !props.is_read,
-                        },
-                        className,
-                    )}
-                >
-                    {image_url ? (
-                        <Image
-                            className='block'
-                            src={image_url}
-                            width={50}
-                            height={50}
-                        />
-                    ) : (
-                        <MdAccountCircle
-                            className='block text-skin-secondary'
-                            size={50}
-                            viewBox='2 2 20 20'
-                        />
-                    )}
+    async function markAsRead() {
+        if (is_read) {
+            return;
+        }
 
-                    <figcaption className='ml-sm'>
-                        <p className='text-md sm:text-sm'>
-                            <b>{name}</b> <span>{props.message}</span>
-                        </p>
-                    </figcaption>
-                </figure>
-            </Link>
+        await axiosClient().put(`/api/notifications/${slug}/read`);
+
+        queryClient.setQueryData<InfiniteData<NotificationPage> | undefined>(
+            'notifications',
+            current => {
+                const notifications = current?.pages.flatMap(page => [
+                    ...page.items,
+                ]);
+
+                notifications?.forEach(n => {
+                    const notification = n;
+
+                    notification.is_read = true;
+                });
+
+                return current;
+            },
         );
-    },
-);
+    }
 
-export default Notification;
+    return (
+        <Link href={url}>
+            <figure
+                ref={ref}
+                className={clsx(
+                    'flex items-center text-skin-primary p-md border border-primary-transparent rounded cursor-pointer',
+                    is_read ? 'bg-primary-transparent' : 'bg-skin-main',
+                    className,
+                )}
+                onClick={markAsRead}
+                aria-hidden
+            >
+                {user.image_url ? (
+                    <Image
+                        className='block'
+                        src={user.image_url}
+                        width={50}
+                        height={50}
+                    />
+                ) : (
+                    <MdAccountCircle
+                        className='block text-skin-secondary'
+                        size={50}
+                        viewBox='2 2 20 20'
+                    />
+                )}
+
+                <figcaption className='ml-sm'>
+                    <p className='text-md sm:text-sm'>
+                        <b>{user.name}</b> <span>{message}</span>
+                    </p>
+                </figcaption>
+            </figure>
+        </Link>
+    );
+}
+
+export default forwardRef(Notification);
